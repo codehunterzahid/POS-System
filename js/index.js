606648a5e0230819830dc946a42e2f82089dacbb
codehunterzahid <codehuntingwithzahid@gmail.com>
2024-03-03 23:44:15 +0500

    changes made

--- js/index.js
+++ js/index.js
@@ -1,57 +1,56 @@
 // this code is to add countdown timer on inventory
 
-document.addEventListener('DOMContentLoaded', function () {
-    let countdownTime = localStorage.getItem('countdownTime') || 8 * 60 * 60;
+document.addEventListener("DOMContentLoaded", function () {
+  let countdownTime = localStorage.getItem("countdownTime") || 8 * 60 * 60;
 
-    let countdownInterval;
+  let countdownInterval;
 
-    const hoursElement = document.querySelector('#hours');
-    const minElement = document.querySelector('#min');
-    const secElement = document.querySelector('#sec');
+  const hoursElement = document.querySelector("#hours");
+  const minElement = document.querySelector("#min");
+  const secElement = document.querySelector("#sec");
 
-    updateCountdown();
+  updateCountdown();
 
-    function updateCountdown() {
-        const hours = Math.floor(countdownTime / 3600);
-        const minutes = Math.floor((countdownTime % 3600) / 60);
-        const seconds = countdownTime % 60;
+  function updateCountdown() {
+    const hours = Math.floor(countdownTime / 3600);
+    const minutes = Math.floor((countdownTime % 3600) / 60);
+    const seconds = countdownTime % 60;
 
-        hoursElement.innerText = hours.toString().padStart(2, '0');
-        minElement.innerText = minutes.toString().padStart(2, '0');
-        secElement.innerText = seconds.toString().padStart(2, '0');
+    hoursElement.innerText = hours.toString().padStart(2, "0");
+    minElement.innerText = minutes.toString().padStart(2, "0");
+    secElement.innerText = seconds.toString().padStart(2, "0");
 
-        localStorage.setItem('countdownTime', countdownTime);
-    }
+    localStorage.setItem("countdownTime", countdownTime);
+  }
 
-    function startCountdown() {
-        countdownInterval = setInterval(function () {
-            countdownTime--;
+  function startCountdown() {
+    countdownInterval = setInterval(function () {
+      countdownTime--;
 
-            updateCountdown();
+      updateCountdown();
 
-            if (countdownTime < 0) {
-                clearInterval(countdownInterval);
-                console.log('Countdown has finished!');
-            }
-        }, 1000);
-    }
-
-    function stopCountdown() {
+      if (countdownTime < 0) {
         clearInterval(countdownInterval);
-    }
+        console.log("Countdown has finished!");
+      }
+    }, 1000);
+  }
 
-    startCountdown();
+  function stopCountdown() {
+    clearInterval(countdownInterval);
+  }
 
+  startCountdown();
 });
 
 // adding functionality to add to cart button
 
-const cartBtn = document.querySelector('.add-to-cart');
-const itemImg = document.querySelector('.item-img');
-const itemPrice = document.querySelector('.item-price');
+const cartBtn = document.querySelector(".add-to-cart");
+const itemImg = document.querySelector(".item-img");
+const itemPrice = document.querySelector(".item-price");
 
-cartBtn.addEventListener('click', function () {
-    itemImg.src = document.querySelector('.item-img').src;
-    itemPrice.innerHTML = document.querySelector('.item-price').innerHTML;
+cartBtn.addEventListener("click", function () {
+  itemImg.src = document.querySelector(".item-img").src;
+  itemPrice.innerHTML = document.querySelector(".item-price").innerHTML;
 });
 
